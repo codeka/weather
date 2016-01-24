eeb53c85d68dfb910e51c7b6893cd8bdd8e83637
Dean Harding <dean@codeka.com.au>
2016-01-24 22:02:43 +1100

    Added a refresh button to the weather map page.

--- app/src/main/java/au/com/codeka/weather/WeatherMapFragment.java
+++ app/src/main/java/au/com/codeka/weather/WeatherMapFragment.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.github.florent37.materialviewpager.MaterialViewPagerHelper;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
@@ -27,18 +28,23 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import au.com.codeka.weather.model.WeatherInfo;
+
 /**
  * Fragment which displays a map with some weather info overlayed over it.
  */
 public class WeatherMapFragment extends Fragment {
+  private FrameLayout refreshButton;
   private ObservableScrollView scrollView;
   private SupportMapFragment mapFragment;
   private GoogleMap map;
 
   private Handler handler = new Handler();
 
+  private boolean overlayLoading;
   private GroundOverlay overlay;
   private LatLngBounds overlayLatLngBounds;
+  private boolean needsReposition;
   private List<Bitmap> overlayBitmaps;
   private int overlayFrame;
 
@@ -49,27 +55,46 @@ public class WeatherMapFragment extends Fragment {
       Bundle savedInstanceState) {
     View rootView = inflater.inflate(R.layout.weather_map_layout, container, false);
     scrollView = (ObservableScrollView) rootView.findViewById(R.id.scroll_view);
+    refreshButton = (FrameLayout) rootView.findViewById(R.id.refresh_button);
 
     mapFragment = new SupportMapFragment();
     FragmentTransaction fragmentTransaction = getChildFragmentManager().beginTransaction();
     fragmentTransaction.add(R.id.map, mapFragment);
     fragmentTransaction.commit();
 
+    refreshButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        refreshOverlay();
+      }
+    });
+    refreshButton.setClickable(false);
+
     mapFragment.getMapAsync(new OnMapReadyCallback() {
       @Override
       public void onMapReady(GoogleMap googleMap) {
-        // TODO: put the camera where we actually think you are
-        LatLng latlng = new LatLng(40.7053111, -74.2581875);
+        // Start off at the weather location
+        WeatherInfo weatherInfo = WeatherManager.i.getCurrentWeather(getActivity());
+        LatLng latlng = new LatLng(weatherInfo.getLat(), weatherInfo.getLng());
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 8.0f));
 
-        overlayLatLngBounds = googleMap.getProjection().getVisibleRegion().latLngBounds;
         map = googleMap;
 
-        new Thread(overlayUpdateRunnable).start();
+        refreshOverlay();
       }
     });
 
     return rootView;
+  }
+
+  /**
+   * Refreshes the overlay so that it's displaying on whatever the map is currently looking at.
+   * Must run on the UI thread.
+   */
+  private void refreshOverlay() {
+    overlayLatLngBounds = map.getProjection().getVisibleRegion().latLngBounds;
+
+    new Thread(overlayUpdateRunnable).start();
   }
 
   @Override
@@ -80,6 +105,9 @@ public class WeatherMapFragment extends Fragment {
   private final Runnable overlayUpdateRunnable = new Runnable() {
     @Override
     public void run() {
+      overlayLoading = true;
+      getActivity().runOnUiThread(updateRefreshButtonRunnable);
+
       InputStream ins = WeatherManager.i.fetchMapOverlay(
           overlayLatLngBounds,
           mapFragment.getView().getWidth() / 2,
@@ -96,6 +124,8 @@ public class WeatherMapFragment extends Fragment {
         overlayBitmaps.add(gifDecoder.getFrame(frame));
       }
 
+      overlayLoading = false;
+      needsReposition = true;
       getActivity().runOnUiThread(updateOverlayFrameRunnable);
     }
   };
@@ -113,11 +143,33 @@ public class WeatherMapFragment extends Fragment {
         overlay.setImage(bitmapDescriptor);
       }
 
+      if (needsReposition) {
+        overlay.setPositionFromBounds(overlayLatLngBounds);
+        needsReposition = false;
+      }
+
       overlayFrame++;
       if (overlayFrame >= overlayBitmaps.size()) {
         overlayFrame = 0;
       }
       handler.postDelayed(updateOverlayFrameRunnable, 1000);
+
+      updateRefreshButtonRunnable.run();
+    }
+  };
+
+  private final Runnable updateRefreshButtonRunnable = new Runnable() {
+    @Override
+    public void run() {
+      if (overlayLoading) {
+        refreshButton.setClickable(false);
+        refreshButton.findViewById(R.id.refresh_progress).setVisibility(View.VISIBLE);
+        refreshButton.findViewById(R.id.refresh_text).setVisibility(View.GONE);
+      } else {
+        refreshButton.setClickable(true);
+        refreshButton.findViewById(R.id.refresh_progress).setVisibility(View.GONE);
+        refreshButton.findViewById(R.id.refresh_text).setVisibility(View.VISIBLE);
+      }
     }
   };
 }
